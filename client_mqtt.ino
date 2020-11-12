71b204b7ecbbf799c323ca4a058eae1f0e78bca7
niklas389 <n1698@outlook.com>
2020-11-12 15:51:15 +0100

    mqtt payload is json; battery state as percentage

--- client_mqtt.ino
+++ client_mqtt.ino
@@ -1,4 +1,5 @@
 // Idea: https://github.com/flazer/weather-station
+#include <ArduinoJson.h>
 #include <BME280I2C.h>
 #include <Wire.h>
 #include <ESP8266WiFi.h>
@@ -16,11 +17,13 @@ PubSubClient client(wifiClient);
 #define SERIAL_BAUD 115200
 #define FORCE_DEEPSLEEP
 
+const String sw_version = "2020.28.6";
+
 BME280I2C bme;
 
 // SETUP
 void setup() {
-    if (DEBUGGING = true) {
+    if (DEBUGGING == true) {
         Serial.begin(SERIAL_BAUD);
         splashScreen();
     }
@@ -32,11 +35,12 @@ void setup() {
     Serial.print("Searching sensor...");
 
     int searchTO = 0;
-    while (!bme.begin()){
+    while (!bme.begin()) {
         searchTO++;
         Serial.print(".");
 
-        if (searchTO > 10) {
+        if (searchTO > 10)
+        {
             Serial.println("");
             Serial.println("Couldn't find BME sensor! >> Deep Sleep");
             searchTO = 0;
@@ -47,37 +51,41 @@ void setup() {
 
     switch (bme.chipModel()) {
     case BME280::ChipModel_BME280:
-        Serial.println("Found BME280 sensor!");   
+        Serial.println("Found BME280 sensor!");
         break;
 
     case BME280::ChipModel_BMP280:
         Serial.println("Found BMP280 sensor! - Humidity not available.");
         break;
-    
+
     default:
         Serial.println("Found unsupported sensor.");
         break;
     }
-    
+
     startWiFi();
 }
 
 void loop() {
     runMQTT();
-    sendMQTTdata();
-    
+    //sendMQTTdata();
+    sendMQTT_v2();
+
     delay(150);
     go_DS(min2sleep);
 }
 
-void sendMQTTdata() {
-    float temp(NAN), hum(NAN), pres(NAN);
-    int bat(NAN), batRAW(NAN);
+void sendMQTT_v2() {
+    float temp(NAN), hum(NAN), pres(NAN), batCalc(NAN);
+    int bat(NAN), batRAW(NAN), batPer(NAN);
     BME280::Mode_Forced;
 
     BME280::TempUnit tempUnit(BME280::TempUnit_Celsius);
     BME280::PresUnit presUnit(BME280::PresUnit_hPa);
     bme.read(pres, temp, hum, tempUnit, presUnit);
+
+    char* tempUnitSymb;
+    if (tempUnit == 0) { tempUnitSymb = "°C"; } else { tempUnitSymb = "°F"; }
 
     // measure battery level
     int rawMeasure = 0;
@@ -86,15 +94,48 @@ void sendMQTTdata() {
         delay(100);
     }
 
+    //Battery % calculating
     batRAW = (rawMeasure / battery_measures);
-    bat = batRAW  * 4.837;
+    Serial.print("A0 RAW: ");
+    Serial.println(batRAW);
 
-    // send data
-    client.publish(topic_temperature, String(temp).c_str(), true);
-    client.publish(topic_humidity, String(hum).c_str(), true);
-    client.publish(topic_pressure, String(int(pres)).c_str(), true);
-    client.publish(topic_battery, String(bat).c_str(), true);
-    client.publish(topic_batteryRAW, String(batRAW).c_str(), true);
+    bat = (batRAW + battery_offset) * ((float)battery_full / 1000);
+    batCalc = (float)battery_full / (float)bat;
+    Serial.print("batCalc: ");
+    Serial.println(batCalc);
+    batPer = 100 - ((batCalc - 1) * 100);
+    Serial.println(batPer);
+
+    StaticJsonDocument<200> doc;
+
+    JsonObject obj_0 = doc.createNestedObject("device");
+    obj_0["friendlyName"] = friendlyName;
+    obj_0["sensorType"] = "BME280";
+    obj_0["version"] = sw_version;
+    obj_0["debugging"] = DEBUGGING;
+    obj_0["battery"] = batPer;
+
+    JsonObject obj_1 = doc.createNestedObject("values");
+    obj_1["temp"] = round(temp*10)/10;
+    obj_1["tempUnit"] = tempUnitSymb;
+    obj_1["hum"] = round(hum*10)/10;
+    obj_1["pres"] = round(pres);
+    obj_1["presUnit"] = "hPa";
+
+    // Start a new line
+    Serial.println();
+    // Generate the prettified JSON and send it to the Serial port.
+    serializeJsonPretty(doc, Serial);
+
+    char buffer[256];
+    serializeJson(doc, buffer);
+    Serial.println();
+
+    if (client.publish(topic_data_hassio, buffer) == true) {
+        Serial.println("Sent message");
+    } else {
+        Serial.println("Error sending message");
+    }
 }
 
 /**
@@ -171,25 +212,29 @@ void runMQTT() {
     }
 }
 
-void go_DS (int minutes) {
-    #ifdef FORCE_DEEPSLEEP
-        Serial.print("Deep Sleep for ");
-        Serial.print(minutes);
-        Serial.println(" minutes.");
+void go_DS(int minutes) {
+#ifdef FORCE_DEEPSLEEP
+    Serial.print("Deep Sleep for ");
+    Serial.print(minutes);
+    Serial.println(" minutes.");
 
-        ESP.deepSleep(minutes * 60 * 1000000);
-        delay(100);
-    #endif
+    ESP.deepSleep(minutes * 60 * 1000000);
+    delay(100);
+#endif
 }
 
 void splashScreen() {
-    for (int i=0; i<=5; i++) Serial.println();
+    for (int i = 0; i <= 5; i++)
+        Serial.println();
     Serial.println("#######################################");
     Serial.println("ESP8266 Weather Station");
     Serial.print("Device Name: ");
     Serial.println(espName);
+    Serial.print("Friendly Name: ");
+    Serial.println(friendlyName);
     Serial.print("Client Version: ");
-    Serial.println(clientVer);
+    Serial.println(sw_version);
     Serial.println("#######################################");
-    for (int i=0; i<2; i++) Serial.println();
+    for (int i = 0; i < 2; i++)
+        Serial.println();
 }
--- config_example.h
+++ config_example.h
@@ -2,6 +2,7 @@
 const char* wifi_ssid           =   "<SSID>";
 const char* wifi_password       =   "<Password>";
 const char* espName             =   "<esp8266_name>";
+const char* friendlyName        =   "<device_name>";
 
 // MQTT Config
 const char* mqtt_server         =   "";
@@ -10,16 +11,13 @@ const char* mqtt_password       =   "";
 int mqtt_port                   =   1883;
 
 // MQTT Topics
-const char* topic_humidity      =   "ws/test/humidity";
-const char* topic_temperature   =   "ws/test/temperature";
-const char* topic_pressure      =   "ws/test/pressure";
-const char* topic_battery       =   "ws/test/battery";
-const char* topic_batteryRAW    =   "ws/test/batteryRAW";
+const char* topic_data_hassio   =   "test/data";
 
 // Deep Sleep
 int min2sleep                   =   10;
 
 // Misc
-String clientVer                =   "1.2";
 int battery_measures            =   5;
-bool DEBUGGING                  =   true;
+int battery_full                =   4130;
+int battery_offset              =   140;
+bool DEBUGGING                  =   false;
